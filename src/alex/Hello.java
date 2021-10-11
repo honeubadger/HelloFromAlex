08c3c1451980677286b85974acd9a3e596dd839f
atnana17 <nananzama@gmail.com>
2021-10-11 18:35:56 +0200

    Added a second line to the file.

--- src/alex/Hello.java
+++ src/alex/Hello.java
@@ -3,6 +3,8 @@ package alex;
 public class Hello {
 
     public static void main(String[] args) {
+
         System.out.println("Hello, my name is Alex");
+        System.out.println("Hello Alex, my name is Nana.");
     }
 }
